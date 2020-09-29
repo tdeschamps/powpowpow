2a40dd319a69161869606be9e573278a7b4e7e22
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2020-09-29 16:41:18 +0200

    :bear: :cat: :owl: :cow:

--- mavicou.rb
+++ mavicou.rb
@@ -1,7 +1,7 @@
 class PowPowPow
     def self.call
         TWILIO_CLIENT_SECRET = 'AC9242e7485ab1940fd303fc3c83b7d7f8'
-        TWILIO_CLEINT_ID = 'AC8401e7485ab1940fd303fc3c83b7d7f8'
+        TWILIO_CLIENT_ID = 'AC8401e7485ab1940fd303fc3c83b7d7f8'
         'Hello'
     end
 end
