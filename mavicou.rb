45404e6febeae23a0c8ccfbbc5f291a852887720
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2020-09-29 15:09:18 +0200

    commit again my twilio API keys like a real douchy douche :dog:

--- mavicou.rb
+++ mavicou.rb
@@ -1,5 +1,7 @@
 class PowPowPow
     def self.call
-        'cmFvdWx0Y2VzdHZyYWltZW50bGVwbHVzZm9ydGNvbnRyZWxhbWF2aWNvdQo='
+        TWILIO_API_KEY = 'AC598svg1a56885638f660652ed4810324'
+        TWILIO_AUTH_TOKEN = 'ce2764ad21d390367H018828cLSO2b0ac'
+        'Hello'
     end
 end
