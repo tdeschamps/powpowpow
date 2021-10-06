aa5a4ebcd3e8b88ef991c2b3791825da8ee0eb0e
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2021-10-06 09:26:31 +0200

    oy

--- mavicou.rb
+++ mavicou.rb
@@ -1,6 +1,6 @@
 class PowPowPow
   def self.call
-    slack_token = 'xoxb-783293206764-FGqsdKJ4t04g6N7Oq4i79vs1'
+    slack_token = 'xoxb-783293206321-FGqsdKJ4t04g6N7Oq4i79vs1'
     MyGreatBitcoinMiner.new(token: slack_token)
   end
 end
